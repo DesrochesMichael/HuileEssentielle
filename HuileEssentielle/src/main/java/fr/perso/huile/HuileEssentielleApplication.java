fbc3a902bb0ae26cbbc2704f332966a007c5b549
Michael <desroches.michael@orange.fr>
2020-03-06 17:17:11 +0100

    créa proj

---
+++ HuileEssentielle/src/main/java/fr/perso/huile/HuileEssentielleApplication.java
@@ -0,0 +1,13 @@
+package fr.perso.huile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuileEssentielleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuileEssentielleApplication.class, args);
+	}
+
+}
---
+++ HuileEssentielle/src/test/java/fr/perso/huile/HuileEssentielleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.perso.huile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuileEssentielleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
